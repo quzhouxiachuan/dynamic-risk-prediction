48ad8900166e996a6ecd490e9cd019fa2f299b4c
quzhouxiachuan <1473230133@qq.com>
2018-09-27 10:21:55 -0500

    Create pooled_risk_imputation.R

---
+++ pooled_risk_imputation.R
@@ -0,0 +1,101 @@
+## reference: https://www.framinghamheartstudy.org/fhs-risk-functions/cardiovascular-disease-10-year-risk/
+library(JMbayes)
+library(plyr)
+library(mice)
+setwd('R:/PrevMed/Faculty/Zhao/Yu/data')
+#setwd('/Volumes/fsmresfiles/PrevMed/Faculty/Zhao/Yu/data')
+load('./LRPP_updated.RData')
+D0$Time = D0$age-D0$base_age
+D1 = D0[D0$COHORT %in% c('FHS OFFSPRING', 'CARDIA', 'CHS',  'ARIC', 'MESA', 'JHS') & D0$ttocvd>0 & D0$male == 0 & D0$RACE == 'White', c("ID_d","ttocvd","Time" ,"cvd", "SBP",'HDLCHL','TOTCHL','HXDIAB','SMOKER','RXHYP','base_age', 'BMINOW')]#D1 = na.omit(D1)
+FHS = D0[D0$COHORT =='FHS ORIGINAL',]
+###HDL measurement does not start until Time 16. Take time 16 as baseline and delete individual who has missing data at time 16 
+FHS = FHS[FHS$Time>=16,]
+FHS = FHS[FHS$ttocvd> 16, c("ID_d","ttocvd","Time" ,"cvd", "SBP",'HDLCHL','TOTCHL','HXDIAB','SMOKER','RXHYP','base_age', 'BMINOW') ]
+
+D1 = rbind(D1, FHS)
+
+#imputed <- mice(D1, m=5, maxit = 5, method = 'pmm', seed = 500)
+#completedData <- complete(imputed,1)
+D1_ = D1[,2:12]
+imputed = mice(D1_)
+complete = complete(imputed, 1)
+Dk=cbind( D1[1], complete)
+D1 = Dk 
+D1$RXHYP_N = 1- D1$RXHYP
+
+D1.id = D1[!duplicated(D1$ID_d),] #sample size 1506 
+#delete rows that have repeated measurements time later than survival time 
+max = ddply(D1, "ID_d", summarize, max = max(ttocvd)) 
+D2 = merge (D1, max, by= 'ID_d' , all.x = T)
+D1 = D2[-2]
+colnames(D1)[13] = 'ttocvd'
+D1 = D1[D1$Time < D1$ttocvd | D1$Time == D1$ttocvd,]
+D1.id = D1[!duplicated(D1$ID_d),] #sample size 1506 
+
+###############set up training set and testing set#####################
+#####training set allows NA values, while testing set does not ########
+set.seed(101) # Set Seed so that same sample can be reproduced in future also
+# Now Selecting 75% of data as sample from total 'n' rows of the data  
+sample <- sample.int(n = nrow(D1.id), size = floor(.75*nrow(D1.id)), replace = F)
+D.id <- D1.id[sample, ]
+D =  D1[D1$ID_d %in% D.id$ID_d,]
+ND.id  <- D1.id[-sample, ]
+ND = D1[D1$ID_d %in% ND.id$ID_d,]
+
+library(SciViews)
+multMixedFit1 <- mvglmer(list(ln(SBP) ~ Time  + (Time | ID_d),
+                              ln(TOTCHL) ~ Time  + (1 | ID_d)), data = D,
+                         families = list(gaussian, gaussian))
+
+#D1.id = D1[!duplicated(D1$ID_d),]
+#to use lg() and ln() function from SciViews package 
+#coxfit1 = coxph(Surv(ttocvd,cvd) ~ ln(base_age)+ (ln(base_age))^2 +ln(TOTCHL)+ ln(base_age): ln(TOTCHL) + ln(HDLCHL) 
+#+ ln(base_age):ln(HDLCHL) + lg(SBP) + ln(base_age):lg(SBP)+SMOKER + ln(base_age):SMOKER+HXDIAB , data=D.id,model = TRUE)
+coxfit1 = coxph(Surv(ttocvd,cvd) ~ ln(base_age)+ (ln(base_age))^2 +ln(TOTCHL)+ ln(base_age): ln(TOTCHL) + ln(HDLCHL) 
++ ln(base_age):ln(HDLCHL) + ln(SBP):RXHYP + ln(SBP):RXHYP_N + SMOKER + ln(base_age):SMOKER + HXDIAB , data=D.id,model = TRUE)
+
+
+multJMFit1 <- mvJointModelBayes(multMixedFit1, coxfit1, timeVar = "Time")
+
+
+## testing model 
+#ND1 = na.omit(ND)
+ND.id = ND[!duplicated(ND$ID_d),]
+
+aucJM(multJMFit1, newdata=ND, Tstart=16, Thoriz = NULL, Dt = 10, idVar = 'ID_d')
+#0.8219 2332
+#make Tstart time greater than the smallest repeated measurement time 
+aucJM(multJMFit1, newdata=ND, Tstart=20, Thoriz = NULL, Dt = 10, idVar = 'ID_d')
+#0.8704  2074
+aucJM(multJMFit1, newdata=ND, Tstart=25, Thoriz = NULL, Dt = 10, idVar = 'ID_d')
+#0.7726  852
+
+##aucJM for simple cox regression model 
+aucJM(coxfit1, newdata= ND.id, idVar = "ID_d", respVar = "cvd", timeVar = "Time", evTimeVar = "ttocvd", Thoriz= 26, Tstart=16)
+#0.6241
+aucJM(coxfit1, newdata= ND.id, idVar = "ID_d", respVar = "cvd", timeVar = "Time", evTimeVar = "ttocvd", Thoriz= 30, Tstart=20)
+#0.6693 (133)
+aucJM(coxfit1, newdata= ND.id, idVar = "ID_d", respVar = "cvd", timeVar = "Time", evTimeVar = "ttocvd", Thoriz= 35, Tstart=25)
+# 0.7316 (81)
+
+
+#extra form 
+#dForm <- list(fixed = ~ 1, random = ~ 1, indFixed = 2, indRandom = 2)
+Forms <- list("ln(SBP)" = "value", "ln(SBP)" = list(fixed = ~ 1, random = ~ 1,
+indFixed = 2, indRandom = 2, name = "slope"))
+multJMFit2 <- update(multJMFit1, Formulas = Forms)
+
+aucJM(multJMFit2, newdata=ND1, Tstart=16, Thoriz = NULL, Dt = 10, idVar = 'ID_d')
+#
+#make Tstart time greater than the smallest repeated measurement time 
+aucJM(multJMFit2, newdata=ND1, Tstart=20, Thoriz = NULL, Dt = 10, idVar = 'ID_d')
+#0.6211
+aucJM(multJMFit2, newdata=ND1, Tstart=25, Thoriz = NULL, Dt = 10, idVar = 'ID_d')
+#0.6481
+
+##aucJM for simple cox regression model 
+aucJM(coxfit1, newdata= ND1.id, idVar = "ID_d", respVar = "cvd", timeVar = "Time", evTimeVar = "ttocvd", Thoriz= 26, Tstart=16)
+#0.6241
+aucJM(coxfit1, newdata= ND1.id, idVar = "ID_d", respVar = "cvd", timeVar = "Time", evTimeVar = "ttocvd", Thoriz= 30, Tstart=20)
+#0.6693 (133)
+aucJM(coxfit1, newdata= ND1.id, idVar = "ID_d", respVar = "cvd", timeVar = "Time", evTimeVar = "ttocvd", Thoriz= 35, Tstart=25)
