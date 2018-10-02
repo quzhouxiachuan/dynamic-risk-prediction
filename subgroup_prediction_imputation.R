0ee56044af0567c60588ec93760233bb616c7fe7
quzhouxiachuan <1473230133@qq.com>
2018-10-02 10:44:39 -0500

    Update subgroup_prediction_imputation.R

--- subgroup_prediction_imputation.R
+++ subgroup_prediction_imputation.R
@@ -128,10 +128,3 @@ aucJM(multJMFit3, newdata=ND, Tstart=25, Thoriz = NULL, Dt = 10, idVar = 'ID_d')
 # 0.7447 (236 subjects still at risk)
 
 
-
-##aucJM for simple cox regression model 
-aucJM(coxfit1, newdata= ND.id, idVar = "ID_d", respVar = "cvd", timeVar = "Time", evTimeVar = "ttocvd", Thoriz= 26, Tstart=16)
-#0.6241
-aucJM(coxfit1, newdata= ND.id, idVar = "ID_d", respVar = "cvd", timeVar = "Time", evTimeVar = "ttocvd", Thoriz= 30, Tstart=20)
-#0.6693 (133)
-aucJM(coxfit1, newdata= ND.id, idVar = "ID_d", respVar = "cvd", timeVar = "Time", evTimeVar = "ttocvd", Thoriz= 35, Tstart=25)
