c44984e286db6d3784e29b599a5b6e34c2010765
quzhouxiachuan <1473230133@qq.com>
2018-10-09 12:04:06 -0500

    Update subgroup_prediction_imputation_frombasedline.R

--- subgroup_prediction_imputation_frombasedline.R
+++ subgroup_prediction_imputation_frombasedline.R
@@ -81,8 +81,11 @@ multJMFit1 <- mvJointModelBayes(multMixedFit1, coxfit1, timeVar = "Time")
 #ND1 = na.omit(ND)
 #ND.id = ND[!duplicated(ND$ID_d),]
 
-aucJM(multJMFit1, newdata=ND, Tstart=16, Thoriz = NULL, Dt = 10, idVar = 'ID_d')
+#aucJM(multJMFit1, newdata=ND, Tstart=16, Thoriz = NULL, Dt = 10, idVar = 'ID_d')
 #0.8944(660)
+
+
+aucJM(multJMFit1, newdata=ND, Tstart=15, Thoriz = NULL, Dt = 10, idVar = 'ID_d')
 #make Tstart time greater than the smallest repeated measurement time 
 aucJM(multJMFit1, newdata=ND, Tstart=20, Thoriz = NULL, Dt = 10, idVar = 'ID_d')
 #0.8947(574)
@@ -114,7 +117,8 @@ aucJM(multJMFit2, newdata=ND, Tstart=25, Thoriz = NULL, Dt = 10, idVar = 'ID_d')
 Forms2 <- list( "ln(SBP)" = list(fixed = ~ 1, random = ~ 1,
           indFixed = 2, indRandom = 2, name = "slope"))
 multJMFit3 <- update(multJMFit1, Formulas = Forms2)
-aucJM(multJMFit3, newdata=ND, Tstart=16, Thoriz = NULL, Dt = 10, idVar = 'ID_d')
+aucJM(multJMFit1, newdata=ND, Tstart=15, Thoriz = NULL, Dt = 10, idVar = 'ID_d')
+#aucJM(multJMFit3, newdata=ND, Tstart=16, Thoriz = NULL, Dt = 10, idVar = 'ID_d')
 # 0.8901 , 542 
 aucJM(multJMFit3, newdata=ND, Tstart=20, Thoriz = NULL, Dt = 10, idVar = 'ID_d')
 # 0.9005 (480 subjects still at risk)
